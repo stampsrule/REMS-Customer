8293726b5e54e96866168da5d13577976832bf7f
stampsrule <stampsrule@gmail.com>
2014-02-25 17:58:35 -0700

    Initial Commit

---
+++ REMS-CustomerTests/REMS_CustomerTests.m
@@ -0,0 +1,34 @@
+//
+//  REMS_CustomerTests.m
+//  REMS-CustomerTests
+//
+//  Created by Daniel Bell on 2/25/2014.
+//  Copyright (c) 2014 Daniel Bell. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface REMS_CustomerTests : XCTestCase
+
+@end
+
+@implementation REMS_CustomerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
